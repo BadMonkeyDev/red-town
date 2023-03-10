a2fce6df0cb08e8fa992f8abcdea43c88072aae1
BadMonkeyDev <arsenii.aivazian@gmail.com>
2023-03-10 12:51:52 +0200

    Added navbar & Link modified classnames added sizes block with header height to global vars added inverted vars to themes added link component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,21 +1,18 @@
 import React from 'react';
-import {Link} from "react-router-dom";
 import {useTheme} from "app/providers/ThemeProvider";
 import {classNames} from "shared/lib/classNames/classNames";
 
 import {AppRouter} from "app/providers/router";
+import {Navbar} from "widgets/Navbar";
 
 const App = () => {
     const {theme, toggleTheme} = useTheme()
 
     return (
         <div className={classNames('app', {}, [theme])}>
-            <button onClick={toggleTheme}>Change Theme</button>
-            <div>
-                <Link to={'/'}>Main</Link>&nbsp;
-                <Link to={'/about'}>About</Link>
-            </div>
+            <Navbar />
             <AppRouter/>
+            <button onClick={toggleTheme}>Change Theme</button>
         </div>
     );
 };
