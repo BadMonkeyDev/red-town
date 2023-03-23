22a5bb4f4a5b73c4e13175784c966b5b3a12de5f
BadMonkeyDev <arsenii.aivazian@gmail.com>
2023-03-23 12:10:53 +0200

    Added and configured redux, added test slice counter

---
+++ src/entities/Counter/ui/Counter.tsx
@@ -0,0 +1,35 @@
+import { AppButton, AppButtonVariant } from 'shared/ui/AppButton/AppButton';
+import { useDispatch, useSelector } from 'react-redux';
+import { getCounterValue } from '../model/selectors/getCounterValue/getCounterValue';
+import { counterActions } from '../model/slice/CounterSlice';
+
+export const Counter = () => {
+    const dispatch = useDispatch();
+    const counterValue = useSelector(getCounterValue);
+
+    const increment = () => {
+        dispatch(counterActions.increment());
+    };
+    const decrement = () => {
+        dispatch(counterActions.decrement());
+    };
+
+    return (
+        <div style={{
+            display: 'flex', flexDirection: 'column', gap: 16, alignItems: 'center',
+        }}
+        >
+            <h1>{counterValue}</h1>
+            <div style={{ display: 'flex', gap: 16 }}>
+                {/* eslint-disable-next-line i18next/no-literal-string */}
+                <AppButton variant={AppButtonVariant.OUTLINED} onClick={increment}>
+                    increment
+                </AppButton>
+                {/* eslint-disable-next-line i18next/no-literal-string */}
+                <AppButton variant={AppButtonVariant.OUTLINED} onClick={decrement}>
+                    decrement
+                </AppButton>
+            </div>
+        </div>
+    );
+};
